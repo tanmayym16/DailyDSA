a3a93d4a96d0204614035cdd34d67dc6f99ffb38
tanmayym16 <mathurtanmay03@gmail.com>
2025-08-03 17:50:57 +0530

    Create best_time_to_buy_&_sell_stocks.cpp

---
+++ Custom 150/Arrays/best_time_to_buy_&_sell_stocks.cpp
@@ -0,0 +1,17 @@
+//Leetcode  121
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxProfit= 0;
+        int bestBuy= prices[0];
+
+        for(int i=1; i<prices.size(); i++) {
+            if (prices[i] > bestBuy) {
+                maxProfit = max(maxProfit, prices[i] - bestBuy);
+            }
+            bestBuy = min(prices[i], bestBuy);
+        }
+        return maxProfit;
+    }
+};
